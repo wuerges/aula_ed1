1a85d9afae544cc75730916064ab933d44871464
aluno-uffs <aluno-uffs@cco-ensino.cco.uffs.edu.br>
2017-05-29 09:21:18 -0300

    tentando implmentar lista ordenada

--- Trabalho2/cadastro.c
+++ Trabalho2/cadastro.c
@@ -36,30 +36,41 @@ void insert_lista(head * h, int idade, char nome[30]){
 	}
 
 //insere 'x' ordenado pelo id na lista apontada por '*l'
-void insert_ordenado(list ** l, int x){
-    list * node = malloc (sizeof(list));
-    list * it, * prev;
+void insert_ordenado(head * h, int idade, char nome[30]){
+    lista * nodo = malloc (sizeof(lista));
+    lista * aux;
 
     //inicia os valores do nodo
-    node->id = x;
+	nodo->id = idade;
+	strcpy(nodo->nome, nome);	
 
     //encontra o lugar do elemento na lista
-    for (prev = NULL, it = *l; (it != NULL) && (it->id < x); it = it->next){
-        prev = it;   
-    } 
-
-    //caso for o primeiro elemento
-    if(prev == NULL){
-        *l = node;
-    //para os outros dois casos
-    }else
-        prev->next = node;
-    
-    
-    node->next = it;
-}
-
-
+    for(aux = h->first; (aux!=NULL) && (aux->id < nodo->id); aux = aux->next){
+		
+		//caso for o primeiro elemento
+		if(aux == NULL){
+			h->last = h->last = nodo;
+			printf("adicionou no primeiro\n");
+			}
+			
+		//caso seja o último elemento
+		else if(aux->next == NULL){
+			aux->next = nodo;
+			nodo->next = NULL;
+			nodo->prev = aux;
+			printf("adicionou no ultimo\n");		
+			}
+		
+		//caso seja inserido no meio
+		else{
+			aux->prev->next = nodo;
+			nodo->prev = aux->prev->next;
+			aux->prev = nodo;
+			nodo->next = aux;
+			printf("adicionou no ultimo\n");		
+			}			
+		}
+	}
 
 void print_lista(head * h){
 	lista * aux;
@@ -77,8 +88,13 @@ int main(){
 	h.first = NULL;
 	h.last = NULL;
 	
-	insert_lista(&h, 30, "José");
-	insert_lista(&h, 25, "Maria");
+	insert_ordenado(&h, 30, "José");
+	insert_ordenado(&h, 25, "Maria");
+	insert_ordenado(&h, 50, "Maria");
+	insert_ordenado(&h, 45, "Maria");
+	
+//	insert_lista(&h, 30, "José");
+//	insert_lista(&h, 25, "Maria");	
 	
 	print_lista(&h);
 	
