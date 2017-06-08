430187dd2333d5dcf29353cdb98320928568ad66
Laura Helena Bento Dacol <laura@localhost.localdomain>
2017-06-08 19:48:05 -0300

    revisao

--- RevisaoProva2/questao3.c
+++ RevisaoProva2/questao3.c
@@ -1,22 +1,37 @@
+/*
+ * 3. Dê um procedimento que inverta uma lista simplesmente ligada de n 
+ * elementos em tempo Θ(n). Em outras palavras, após inverter, os 
+ * ponteiros devem apontar para trás.
+ */
+
 #include<stdio.h>
 #include<stdlib.h>
 
 typedef struct lista{
 	int id;
-	lista * next;	
-}lista;.
+	struct lista * next;	
+}lista;
 
 void insert(lista * p, int id){
 	lista * nodo = malloc(sizeof(lista));
 	lista * aux;
-	
+
 	nodo->id = id;
 	nodo->next = NULL;		
 	
 	for(aux = p; aux->next!=NULL; aux = aux->next){
 		}
-	
-	aux->next = nodo;	
+
+	if(aux == p){
+		p = nodo;
+		printf("inseriu no começo\n");
+		}
+		
+	else{
+		aux->next = nodo;
+		printf("inseriu no meio\n");		
+		}
+
 	}
 	
 void print(lista * p){
@@ -31,6 +46,19 @@ void print(lista * p){
 int main(){
 
 	lista * p;
+	
+	p = NULL;
+
+	insert(p, 1);
+	insert(p, 2);
+	insert(p, 3);
+	
+	print(p);
+	
+
+
+	
+	
 
 
 return 0;
